765d420f517c9f0909374115f0c989429f38d43c
KiRiliUSS <KiRiliUSS@MAIL.RU>
2018-06-25 23:39:44 +0300

    add new test yml

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,25 +3,21 @@ import path from 'path';
 import genDiff from '../src/genDiff';
 
 const buildFixturePath = fileName => path.join(__dirname, '__fixtures__', fileName);
+const expectedDiffPath = buildFixturePath('expected');
+const fileExpected = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
 
 describe('Test Difference Calculator!', () => {
   it('Two JSON', () => {
-    const expectedDiffPath = buildFixturePath('expected');
-    const fileExpected = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
     const fixtures = ['before.json', 'after.json'];
     const [file1Path, file2Path] = fixtures.map(buildFixturePath);
     expect(genDiff(file1Path, file2Path)).toEqual(fileExpected);
   });
   it('Two YAML', () => {
-    const expectedDiffPath = buildFixturePath('expected');
-    const fileExpected = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
     const fixtures = ['before.yml', 'after.yml'];
     const [file1Path, file2Path] = fixtures.map(buildFixturePath);
     expect(genDiff(file1Path, file2Path)).toEqual(fileExpected);
   });
   it('First JSON, second YAML', () => {
-    const expectedDiffPath = buildFixturePath('expected');
-    const fileExpected = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
     const fixtures = ['before.json', 'after.yml'];
     const [file1Path, file2Path] = fixtures.map(buildFixturePath);
     expect(genDiff(file1Path, file2Path)).toEqual(fileExpected);
