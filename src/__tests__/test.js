a026857ab0bbb4901b050bfd2d050ef5672ff6c0
KiRiliUSS <KiRiliUSS@MAIL.RU>
2018-06-25 15:24:53 +0300

    second step

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -9,7 +9,7 @@ describe('Test Difference Calculator!', () => {
     const expectedDiffPath = buildFixturePath('expected');
     const fileExpected = fs.readFileSync(expectedDiffPath, 'utf-8').trim();
     const fixtures = ['before.json', 'after.json'];
-    const [file1Path, file2Path] = fixtures.map(buildFixturePath);;
+    const [file1Path, file2Path] = fixtures.map(buildFixturePath);
     expect(genDiff(file1Path, file2Path)).toEqual(fileExpected);
   });
 });
