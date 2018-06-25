b2d49f5cf87b48e06a7eab10b2e8a7a6b5274945
KiRiliUSS <KiRiliUSS@MAIL.RU>
2018-06-25 15:21:10 +0300

    second step

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '../genDiff';
 import { version } from '../../package.json';
 
 program
@@ -8,6 +9,6 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    console.info(firstConfig, secondConfig);
+    console.info(genDiff(firstConfig, secondConfig));
   })
   .parse(process.argv);
